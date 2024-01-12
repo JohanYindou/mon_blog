28f4a0e22b9fa6091c7829e5dece1661799306bf
Johan Yindou <jyindou@gmail.com>
2024-01-12 15:56:13 +0100

    Add a sample post fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,24 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Post;
+use DateTimeImmutable;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $post = new Post();
+        $post->setTitle('Mon premier article')
+            ->setSlug('mon-premier-article')
+            ->setContent('Ceci est le contenu de mon premier article')
+            ->setImage('https://via.placeholder.com/350x150')
+            ->setCreatedAt(new DateTimeImmutable('now'))
+            ->setUpdatedAt(new DateTimeImmutable('now'))
+            ;
+        $manager->persist($post);
         $manager->flush();
     }
 }
