1de3fb9aec4f8b7a2bf6ee80f5b4ca7791ee98b2
Johan Yindou <jyindou@gmail.com>
2024-01-12 16:16:59 +0100

    Add 50 fake posts to the database using Faker library.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,20 +6,24 @@ use App\Entity\Post;
 use DateTimeImmutable;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $post = new Post();
-        $post->setTitle('Mon premier article')
-            ->setSlug('mon-premier-article')
-            ->setContent('Ceci est le contenu de mon premier article')
+        $faker = Factory::create('fr-FR');
+        for ($i = 0; $i < 50; $i++) {
+            $post = new Post();
+            $post->setTitle($faker->sentence())
+            ->setSlug($faker->slug())
+            ->setContent($faker->paragraph(5,true))
             ->setImage('https://via.placeholder.com/350x150')
-            ->setCreatedAt(new DateTimeImmutable('now'))
-            ->setUpdatedAt(new DateTimeImmutable('now'))
-            ;
-        $manager->persist($post);
+            ->setCreatedAt(new DateTimeImmutable('1 month ago'))
+            ->setUpdatedAt(new DateTimeImmutable('1 week ago')); 
+            $manager->persist($post);
+        }
+        
         $manager->flush();
     }
 }
